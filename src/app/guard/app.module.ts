aed015615479d98bdab236b14a9da3f1e14d0efc
Benjamin-Ju <benjaminyanju@gmail.com>
2019-11-11 07:05:07 -0500

    added additional components, organized components, add home.html

--- src/app/guard/app.module.ts
+++ src/app/guard/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from '../app-routing.module';
+import { AppComponent } from '../app.component';
+import { HomeComponent } from '../home/home.component';
+import { RegisterComponent } from '../register/register.component';
+import { LoginComponent } from '../login/login.component';
 
 @NgModule({
   declarations: [
